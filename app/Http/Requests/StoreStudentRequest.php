d4c4eba96f19c3402f3200a52dca6cb1f9849608
Qerakl <german111474457@gmail.com>
2024-12-01 00:00:35 +0500

    feat: add request StoreStudentRequest

--- app/Http/Requests/StoreStudentRequest.php
+++ app/Http/Requests/StoreStudentRequest.php
@@ -19,10 +19,34 @@ class StoreStudentRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'login' => 'required|string|unique:students,login|max:255',
+            'password' => 'required|string|min:6|max:255',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'first_name.required' => 'Имя обязательно.',
+            'first_name.string' => 'Имя должно быть строкой.',
+            'first_name.max' => 'Имя не может быть длиннее 255 символов.',
+            'last_name.required' => 'Фамилия обязательна.',
+            'last_name.string' => 'Фамилия должна быть строкой.',
+            'last_name.max' => 'Фамилия не может быть длиннее 255 символов.',
+            'group_id.exists' => 'Выбранная группа недействительна.',
+            'login.required' => 'Логин обязателен.',
+            'login.string' => 'Логин должен быть строкой.',
+            'login.unique' => 'Этот логин уже занят.',
+            'login.max' => 'Логин не может быть длиннее 255 символов.',
+            'password.required' => 'Пароль обязателен.',
+            'password.string' => 'Пароль должен быть строкой.',
+            'password.min' => 'Пароль должен быть не менее 6 символов.',
+            'password.max' => 'Пароль не может быть длиннее 255 символов.',
         ];
     }
 }
