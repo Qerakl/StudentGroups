ebf20da3829170a9439e3af31850224e89d2d28b
Qerakl <german111474457@gmail.com>
2024-12-02 02:33:29 +0500

    feat: add test UpdateTest in Journal and update route

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::resource('group', \App\Http\Controllers\GroupController::class)->except([
 Route::resource('subject', \App\Http\Controllers\SubjectController::class)->except(['create', 'edit']);
 Route::resource('journal', \App\Http\Controllers\JournalController::class)->except(['show', 'create', 'edit', 'update', 'destroy']);
 Route::get('journal/group/{group}', [\App\Http\Controllers\JournalController::class, 'show_group'])->name('journal.show_group');
-Route::put('journal/{studentSubject}', [\App\Http\Controllers\JournalController::class, 'update']);
-Route::delete('journal/{studentSubject}', [\App\Http\Controllers\JournalController::class, 'destroy']);
+Route::put('journal/{studentSubject}', [\App\Http\Controllers\JournalController::class, 'update'])->name('journal.update');
+Route::delete('journal/{studentSubject}', [\App\Http\Controllers\JournalController::class, 'destroy'])->name('journal.destroy');
---
+++ tests/Feature/Journal/UpdateTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\Journal;
+
+use App\Models\Student;
+use App\Models\StudentSubject;
+use App\Models\Subject;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UpdateTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /**
+     * Тест успешного обновления оценки студента по предмету.
+     */
+    public function test_update_student_subject_successfully()
+    {
+        $this->artisan('db:seed');
+        $studentSubject = StudentSubject::first();
+
+        $studentSubject = StudentSubject::where('student_id', $studentSubject->student_id)
+            ->where('subject_id', $studentSubject->subject_id)
+            ->first();
+
+        $response = $this->put(route('journal.update', $studentSubject), [
+            'grade' => 3,
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJsonFragment([
+            'message' => 'StudentSubject updated successfully',
+        ]);
+        $response->assertJsonFragment([
+            'grade' => 3,
+        ]);
+    }
+}
