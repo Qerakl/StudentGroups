63a9d463675625d6c89b09e6d62bb80312cf32ec
Qerakl <german111474457@gmail.com>
2024-12-02 02:24:13 +0500

    feat: add test ShowGroupTest in Journal and update route

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::resource('student', \App\Http\Controllers\StudentController::class)->exce
 Route::resource('group', \App\Http\Controllers\GroupController::class)->except(['create', 'edit']);
 Route::resource('subject', \App\Http\Controllers\SubjectController::class)->except(['create', 'edit']);
 Route::resource('journal', \App\Http\Controllers\JournalController::class)->except(['show', 'create', 'edit', 'update', 'destroy']);
-Route::get('journal/group/{group}', [\App\Http\Controllers\JournalController::class, 'show_group']);
+Route::get('journal/group/{group}', [\App\Http\Controllers\JournalController::class, 'show_group'])->name('journal.show_group');
 Route::put('journal/{studentSubject}', [\App\Http\Controllers\JournalController::class, 'update']);
 Route::delete('journal/{studentSubject}', [\App\Http\Controllers\JournalController::class, 'destroy']);
---
+++ tests/Feature/Journal/ShowGroupTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Journal;
+
+use App\Models\Student;
+use App\Models\Group;
+use App\Models\StudentSubject;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ShowGroupTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /**
+     * Тест успешного получения журнала группы.
+     */
+    public function test_show_group_returns_students_journal_successfully()
+    {
+        $group = Group::factory()->create();
+        $student1 = Student::factory()->create(['group_id' => $group->id]);
+        $student2 = Student::factory()->create(['group_id' => $group->id]);
+        $this->artisan('db:seed');
+
+        $response = $this->getJson(route('journal.show_group', $group));
+        $response->assertStatus(200);
+        $response->assertJsonCount(2);
+    }
+}
