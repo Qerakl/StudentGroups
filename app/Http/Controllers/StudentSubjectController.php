a6989780db1b69b39095019c1afe4453b6779b2f
Qerakl <german111474457@gmail.com>
2024-12-01 22:24:59 +0500

    add method index in controller StudentSubjectController

--- app/Http/Controllers/StudentSubjectController.php
+++ app/Http/Controllers/StudentSubjectController.php
@@ -21,14 +21,6 @@ class StudentSubjectController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreStudentSubjectRequest $request)
