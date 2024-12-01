5c2776b3ba36bae6569e92b6eac4f54912441bd6
Qerakl <german111474457@gmail.com>
2024-12-01 21:38:02 +0500

    add resource route journal

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::resource('student', \App\Http\Controllers\StudentController::class);
 Route::resource('group', \App\Http\Controllers\GroupController::class);
 Route::resource('subject', \App\Http\Controllers\SubjectController::class);
+Route::resource('journal', \Database\Seeders\StudentSubjectSeeder::class);
