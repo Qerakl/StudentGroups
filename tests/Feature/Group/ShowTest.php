aac2595ab33743fc8adfe0f7771e4144a2174795
Qerakl <german111474457@gmail.com>
2024-12-01 19:38:09 +0500

    feat: add test Group\ShowTest

--- tests/Feature/Group/ShowTest.php
+++ tests/Feature/Group/ShowTest.php
@@ -26,4 +26,14 @@ class ShowTest extends TestCase
             'name' => $group->name,
         ]);
     }
+
+    /**
+     * Тест получения данных о несуществующей группе (404).
+     */
+    public function test_show_returns_404_for_nonexistent_group()
+    {
+        $response = $this->get(route('group.show', 9999));
+
+        $response->assertStatus(404);
+    }
 }
