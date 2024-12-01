7288170128b079c371e86cf4e18d490711882462
Qerakl <german111474457@gmail.com>
2024-12-02 00:18:07 +0500

    add request UpdateStudentSubjectRequest

--- app/Http/Requests/UpdateStudentSubjectRequest.php
+++ app/Http/Requests/UpdateStudentSubjectRequest.php
@@ -11,7 +11,7 @@ class UpdateStudentSubjectRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,27 @@ class UpdateStudentSubjectRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'student_id' => 'required|exists:students,id',
+            'subject_id' => 'required|exists:subjects,id',
+            'grade' => 'required|integer|min:2|max:5',
+            'date' => 'required|date|date_format:Y-m-d',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'student_id.required' => 'Поле "Студент" обязательно для заполнения.',
+            'student_id.exists' => 'Указанный студент не существует.',
+            'subject_id.required' => 'Поле "Предмет" обязательно для заполнения.',
+            'subject_id.exists' => 'Указанный предмет не существует.',
+            'grade.required' => 'Поле "Оценка" обязательно для заполнения.',
+            'grade.integer' => 'Оценка должна быть числом.',
+            'grade.min' => 'Оценка должна быть не меньше 2.',
+            'grade.max' => 'Оценка должна быть не больше 5.',
+            'date.required' => 'Поле "Дата" обязательно для заполнения.',
+            'date.date' => 'Неверный формат даты.',
+            'date.date_format' => 'Дата должна быть в формате ГГГГ-ММ-ДД.',
         ];
     }
 }
