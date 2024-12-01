7bf75ef0516c435b3dc75a4908302e829efab69c
Qerakl <german111474457@gmail.com>
2024-12-01 17:15:49 +0500

    update migrate groups add defoault value in count_students

--- database/migrations/2024_11_30_153552_create_groups_table.php
+++ database/migrations/2024_11_30_153552_create_groups_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedInteger('count_students');
+            $table->unsignedInteger('count_students')->default(0);
             $table->timestamps();
         });
     }
