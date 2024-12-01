674f4275b578936abf973d219c7a2c285d6dbcfb
Qerakl <german111474457@gmail.com>
2024-12-01 20:55:05 +0500

    feat: add test Subject\Validation\StoreValidationTest

---
+++ tests/Feature/Subject/Validation/StoreValidationTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature\Subject\Validation;
+
+use App\Models\Subject;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class StoreValidationTest extends TestCase
+{
+
+    use RefreshDatabase, WithFaker;
+
+    /**
+     * Тест валидации на обязательное поле.
+     */
+    public function test_validation_name_is_required()
+    {
+        $response = $this->post(route('subject.store'), [
+            'name' => null,
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['name']);
+    }
+
+    /**
+     * Тест валидации на уникальность имени.
+     */
+    public function test_validation_name_is_unique()
+    {
+        $name = $this->faker->word;
+        Subject::factory()->create(['name' => $name]);
+
+        $response = $this->post(route('subject.store'), [
+            'name' => $name,
+        ]);
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['name']);
+    }
+}
