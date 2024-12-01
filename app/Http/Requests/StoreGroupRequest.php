224a49afb5a4373a971a9faf84d7078c283e64ce
Qerakl <german111474457@gmail.com>
2024-12-01 16:46:39 +0500

    feat: add request StoreGroupRequest

--- app/Http/Requests/StoreGroupRequest.php
+++ app/Http/Requests/StoreGroupRequest.php
@@ -22,7 +22,16 @@ class StoreGroupRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255|unique:groups',
+        ];
+    }
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Поле "Название группы" обязательно для заполнения.',
+            'name.string' => 'Поле "Название группы" должно быть строкой.',
+            'name.max' => 'Поле "Название группы" не должно превышать 255 символов.',
+            'name.unique' => 'Группа с таким названием уже существует.',
         ];
     }
 }
