50be037e8311e0e402c541290edbd7ba0a3ed3c4
Qerakl <german111474457@gmail.com>
2024-12-01 20:03:23 +0500

    feat: add test Student\DestroyTest

--- tests/Feature/Student/DestroyTest.php
+++ tests/Feature/Student/DestroyTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Student;
 
+use App\Models\Group;
 use App\Models\Student;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -11,10 +12,34 @@ class DestroyTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
 
-    public function test_destroy(){
+    /**
+     * Тест успешного удаления студента.
+     */
+    public function test_destroy_deletes_student_successfully(){
         $student = Student::factory()->create();
         $response = $this->delete(route('student.destroy', $student));
 
         $response->assertStatus(200);
+        $this->assertDatabaseMissing('students', [
+            'id' => $student->id
+        ]);
+    }
+    /**
+     * Тест успешного удаления студента и проверка декримента в группе после его удаления.
+     */
+    public function test_destroy_deletes_student_successfully_and_check_decrement(){
+        $this->artisan('db:seed');
+        $group = Group::first();
+        $student = $group->students()->first();
+
+        $response = $this->delete(route('student.destroy', $student));
+        $response->assertStatus(200);
+        $this->assertDatabaseMissing('students', [
+            'id' => $student->id
+        ]);
+        $this->assertDatabaseHas('groups', [
+            'id' => $group->id,
+            'count_students' => $group->count_students - 1,
+        ]);
     }
 }
